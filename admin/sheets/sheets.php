50e97decec14a285f1957eb4bd4de89b5b266172
Luis Guzman <luisg0015@gmail.com>
2014-07-12 13:36:36 -0400

    Adding FPDF & FPDI Library. New sheets.php using new templates.

--- admin/sheets/sheets.php
+++ admin/sheets/sheets.php
@@ -1,210 +1,122 @@
 <?php
-
 session_start();
 
 include '../../include/mysql.php';
 
+ini_set('display_errors', 'On');
+error_reporting(E_ALL);
+
+// Add classes for pdf generating
+require('../../lib/fpdf/fpdf.php');
+require('../../lib/fpdf/fpdi.php');
+
 $result = mysql_query("
-SELECT * FROM match_schedule
-JOIN division ON division.division_id=match_schedule.division_id
-JOIN league ON league.league_id=division.league_id 
-WHERE match_schedule.division_id='{$_GET['division_id']}' AND match_schedule.match_number='{$_GET['week']}'");
+	SELECT * FROM match_schedule
+	JOIN division ON division.division_id=match_schedule.division_id
+	JOIN league ON league.league_id=division.league_id
+	WHERE match_schedule.division_id='{$_GET['division_id']}' AND match_schedule.match_number='{$_GET['week']}'"
+	);
 
 $row = mysql_fetch_assoc($result);
 
 $league_id = $row['league_id'];
 
-switch($row['league_id'])
-{
+// Set temlate and positions
+switch ($row['league_id']) {
+	// Scramble Leagues
 	case '4':
-		$path				= 'background/mixed.jpg';
-		$handicap_DB		= 'handicap_mixed_9';
-		$page['width']		= 612;
-		$page['height']		= 792;
-		$back['x']			= 30;
-		$back['y']			= -240;
-		$back['width']		= 550;
-		$back['height']		= 1000;
-		$player_page		= 1;
-		$player_start_row	= 663;
-		$player_spacing		= 2;
-		$player_font_size	= 9;
-		$player['home']		= $home_team_column = 40;
-		$player['visit']	= $visit_team_column = 355;
-		$team_row			= 690;
-		$date['column']		= 35;
-		$date['row']		= 717;
-	break;
-	
-	case '3':
-		die('This league requires no scoresheet');
-	break;
-			
+		$league_type = 'Scramble';
+		$handicap_DB = 'handicap_mixed_9';
+		$template = 'templates/Scramble_Scoresheet.pdf';
+		$team_home_x = 14;
+		$team_home_y = 56;
+		$team_away_x = 110;
+		$team_away_y = 56;
+		$team_home_x2 = 14;
+		$team_home_y2 = 50;
+		$team_away_x2 = 110;
+		$team_away_y2 = 50;
+		$players_home_x = 14;
+		$players_home_y = 71;
+		$players_away_x = 110;
+		$players_away_y = 71;
+		$game_date_x = 14;
+		$game_date_y = 37;
+		break;
+ 	// 8 Ball League
 	case '2':
-		$path				= 'background/8-ball.jpg';
-		$handicap_DB		= 'handicap_eight';
-		$page['width']		= 612;
-		$page['height']		= 792;
-		$back['x']			= 0;
-		$back['y']			= 20;
-		$back['width']		= 612;
-		$back['height']		= 782;		
-		$player_page		= 2;
-		$player_start_row	= 663;
-		$player_spacing		= 2;
-		$player_font_size	= 9;
-		$home_team_column	= 60;
-		$visit_team_column	= 335;
-		$player['home']		= 110;
-		$player['visit']	= 365;
-		$team_row			= 710;
-		$date['column']		= 335;
-		$date['row']		= 680;		
-	break;
-
-	case '6':
-		$path				= 'background/8-ball.jpg';
-		$handicap_DB		= 'handicap_eight_beginner';
-		$page['width']		= 612;
-		$page['height']		= 792;
-		$back['x']			= 0;
-		$back['y']			= 20;
-		$back['width']		= 612;
-		$back['height']		= 782;		
-		$player_page		= 2;
-		$player_start_row	= 663;
-		$player_spacing		= 2;
-		$player_font_size	= 9;
-		$home_team_column	= 60;
-		$visit_team_column	= 335;
-		$player['home']		= 110;
-		$player['visit']	= 365;
-		$team_row			= 710;
-		$date['column']		= 335;
-		$date['row']		= 680;		
-	break;
-				
+		$league_type = '8-Ball';
+		$handicap_DB = 'handicap_eight';
+		$template = 'templates/8Ball_Scoresheet.pdf';
+		$team_home_x = 14;
+		$team_home_y = 56.5;
+		$team_away_x = 110;
+		$team_away_y = 56.5;
+		$team_home_x2 = 14;
+		$team_home_y2 = 52;
+		$team_away_x2 = 110;
+		$team_away_y2 = 52;
+		$players_home_x = 14;
+		$players_home_y = 68;
+		$players_away_x = 110;
+		$players_away_y = 68;
+		$game_date_x = 14;
+		$game_date_y = 37;
+		break;
+	//9 Ball League
 	case '1':
-		$path				= 'background/9-ball.jpg';
-		$handicap_DB		= 'handicap';
-		$page['width']		= 792;
-		$page['height']		= 612;
-		$back['x']			= 0;
-		$back['y']			= 20;
-		$back['width']		= 782;
-		$back['height']		= 612;	
-		$player_page		= 1;
-		$player_start_row	= 480;
-		$player_spacing		= 10.3;
-		$player_font_size	= 12;
-		$home_team_column	= 90;
-		$visit_team_column	= 445;
-		$player['home']		= 98;
-		$player['visit']	= 455;
-		$team_row			= 507;
-		$date['column']		= 90;
-		$date['row']		= 520;				
-	break;
-			
+		$league_type = '9-Ball';
+		$handicap_DB = 'handicap';
+		$template = 'templates/9Ball_Scoresheet.pdf';
+		$team_home_x = 30;
+		$team_home_y = 55;
+		$team_away_x = 159;
+		$team_away_y = 55;
+		$players_home_x = 30;
+		$players_home_y = 69;
+		$players_away_x = 159;
+		$players_away_y = 69;
+		$game_date_x = 30;
+		$game_date_y = 37;
+		break;
+	//set default error out
 	default:
-		die('This league requires no scoresheet');
-	break;
+		die ('This league does not require scoresheet');
+		break;
 }
 
 $result = mysql_query("
-SELECT *, DATE_FORMAT(match_schedule.match_start_date, '%W - %M %D') week,
-ht.name home_team, vt.name visit_team, ht.team_id home_id, vt.team_id visit_id, league.league_type
-FROM match_schedule
-LEFT JOIN team ht ON ht.team_id=match_schedule.home_team_id
-LEFT JOIN team vt ON vt.team_id=match_schedule.visit_team_id
-JOIN division ON division.division_id=match_schedule.division_id
-JOIN league ON league.league_id=division.league_id 
-WHERE match_schedule.division_id='{$_GET['division_id']}' AND match_schedule.match_number='{$_GET['week']}'");
-	
-try
-{
-	$p = new PDFlib();
-	$p->begin_document('', "");
-	$p->set_parameter("escapesequence", "true");
-	$p->set_parameter("kerning", "true");
-	$p->set_parameter("SearchPath", $_SERVER['DOCUMENT_ROOT'] . 'admin/sheets/fonts');
-}
-catch (Exception $e)
-{
-	die('exception::'.$e);	
-}
+	SELECT *, DATE_FORMAT(match_schedule.match_start_date, '%W - %M %D') week, ht.name home_team, vt.name visit_team, ht.team_id home_id, vt.team_id visit_id, league.league_type
+	FROM match_schedule
+	LEFT JOIN team ht ON ht.team_id=match_schedule.home_team_id
+	LEFT JOIN team vt ON vt.team_id=match_schedule.visit_team_id
+	JOIN division ON division.division_id=match_schedule.division_id
+	JOIN league ON league.league_id=division.league_id 
+	WHERE match_schedule.division_id='{$_GET['division_id']}' AND match_schedule.match_number='{$_GET['week']}'"
+	);
 
-while($row = mysql_fetch_assoc($result))
-{
-	$p->begin_page_ext($page['width'], $page['height'], '');
+// initiate FPDI
+$pdf = new FPDI();
 
-	try
-	{
-		$option = "matchbox={name=image_pos} boxsize={".$back['width'].' '.$back['height']."} fitmethod=meet position={left top}";
-	
-		$image = $p->load_image("auto", $path, "");
-		$p->fit_image($image, $back['x'], $back['y'], $option);
-		$p->close_image($image);
-	
-		$font = $p->load_font("Helvetica", "winansi", "");
-		
-		$font_size = 10;
-		$p->setfont($font, $font_size);
-		$p->set_text_pos($date['column'], $date['row']);
-		$p->setcolor("both", "cmyk", 1, 1, 0, 0);
-		$p->show('Scheduled Date:  '.$row['week']);
-				
-		$p->setfont($font, $font_size);
-		$p->setcolor("both", "cmyk", 0, 0, 0, 1);
-		$p->set_text_pos($home_team_column, $team_row);
-		$p->show($row['home_team']);
-
-		$p->setcolor("both", "cmyk", 0, 0, 0, 1);
-		$p->set_text_pos($visit_team_column, $team_row);
-		$p->show($row['visit_team']);
-		
-		if ($player_page == 2)
-		{
-			$p->end_page_ext("");
-			$p->begin_page_ext($page['width'], $page['height'], '');
-		}
-				
-		$hp_result = mysql_query("
-		SELECT *, CONCAT(player.first_name,' ',player.last_name,' (',handicap_display.hcd_name,')') player_name 
-		FROM team_player 
+while ($row = mysql_fetch_assoc($result)) {
+	// get sql of home team players
+	$hp_result = mysql_query("
+		SELECT *, CONCAT('(',handicap_display.hcd_name,') ', player.first_name,' ',player.last_name) player_name 
+		FROM team_player
 		JOIN player ON player.player_id=team_player.tp_player
 		JOIN team ON team.team_id=team_player.tp_team
 		LEFT JOIN handicap_display ON (hcd_league={$league_id} AND hcd_handicap={$handicap_DB})
-		WHERE 
-		team_player.tp_team='{$row['home_id']}' AND 
+		WHERE team_player.tp_team='{$row['home_id']}' AND 
 		team_player.tp_division='{$_GET['division_id']}' AND 
 		NOT player.first_name LIKE '%FORFEIT%' AND
 		NOT player.last_name LIKE '%FORFEIT%' AND
 		NOT player.first_name LIKE '%HANDICAP%' AND
-		NOT player.last_name LIKE '%HANDICAP%'");
-		
-		$x_position = $player_start_row;
-		$team_printed = false;
-		
-		while($hp_row = mysql_fetch_assoc($hp_result))
-		{
-			if (($player_page == 2) && (!$team_printed))
-			{
-				$p->setfont($font, 16);
-				$p->set_text_pos( $player['home'], $x_position);
-				$p->show($hp_row['name']);
-				$x_position -= 18;
-				$team_printed = true;
-			}
-		
-			$p->setfont($font, $player_font_size);
-			$p->set_text_pos( $player['home'], $x_position);
-			$p->show($hp_row['player_name']);
-			$x_position -= $font_size + $player_spacing;
-		}
-		
-		$hp_result = mysql_query("
-		SELECT *, CONCAT(player.first_name,' ',player.last_name,' (',handicap_display.hcd_name,')') player_name 
+		NOT player.last_name LIKE '%HANDICAP%'"
+		); 
+		  
+	// get sql of visiting team players
+	$vp_result = mysql_query("
+		SELECT *, CONCAT('(',handicap_display.hcd_name,') ', player.first_name,' ',player.last_name) player_name 
 		FROM team_player 
 		JOIN player ON player.player_id=team_player.tp_player
 		JOIN team ON team.team_id=team_player.tp_team
@@ -214,42 +126,209 @@ while($row = mysql_fetch_assoc($result))
 		NOT player.first_name LIKE '%FORFEIT%' AND
 		NOT player.last_name LIKE '%FORFEIT%' AND
 		NOT player.first_name LIKE '%HANDICAP%' AND
-		NOT player.last_name LIKE '%HANDICAP%'");
+		NOT player.last_name LIKE '%HANDICAP%'"
+		); 
+
+	// set the source file
+	$pages= $pdf->setSourceFile($template);
+
+	// Scramble League Scoresheet
+	if ($league_type == "Scramble") {
+	
+		// add a page
+		$pdf->AddPage();
+
+		// import page 1
+		$tplIdx = $pdf->importPage(1);
+
+		// use the imported page
+		$pdf->useTemplate($tplIdx, 0, 0, 216);
+
+		// now write some text above the imported page
+		$pdf->SetFont('Helvetica', 'B', 10);
+		$pdf->SetTextColor(0, 0, 0);
+
+		// write game date
+		$pdf->SetFont('Helvetica', 'B', 10);
+		$pdf->SetXY($game_date_x, $game_date_y);
+		$pdf->Write(0,  $row['week']);
+
+		// write home team
+		$pdf->SetFont('Helvetica', 'B', 12);
+		$pdf->SetXY($team_home_x, $team_home_y);
+		$pdf->Write(0, $row['home_team']);
+
+		// write 8 or 9 ball
+		$pdf->SetFont('Helvetica', 'B', 23);
+		$pdf->SetXY(178, 9.5);
+		$pdf->Cell(50, 30, 'BALL');
 		
-		$x_position = $player_start_row;
-		$team_printed = false;
+		// write home team list
+		$pl_home_y = $players_home_y;
+		while ($hp_row = mysql_fetch_assoc($hp_result)) {
+			$pdf->SetFont('Helvetica', '', 10);
+			$pdf->SetXY($players_home_x, $pl_home_y);
+			$pdf->Write(0, $hp_row['player_name']);
+			$pl_home_y = $pl_home_y + 5; 
+		}
+
+		// write away team
+		$pdf->SetFont('Helvetica', 'B', 12);
+		$pdf->SetXY($team_away_x, $team_away_y);
+		$pdf->Write(0, $row['visit_team']);
+
+		// write away team list
+		$vl_home_y = $players_away_y;
+		while ($vp_row = mysql_fetch_assoc($vp_result)) {
+			$pdf->SetFont('Helvetica', '', 10);
+			$pdf->SetXY($players_away_x, $vl_home_y);
+			$pdf->Write(0, $vp_row['player_name']);
+			$vl_home_y = $vl_home_y + 5; 
+		}
+
+		// add a page
+		$pdf->AddPage();
+
+		// import page 2
+		$tplIdx2 = $pdf->importPage(2);
+		$pdf->useTemplate($tplIdx2, 0, 0, 216);
+
+		// write 8 or 9 ball
+		$pdf->SetFont('Helvetica', 'B', 23);
+		$pdf->SetXY(178, 9.5);
+		$pdf->Cell(50, 30, 'BALL');
+
+		// write home team
+		$pdf->SetFont('Helvetica', 'B', 12);
+		$pdf->SetXY($team_home_x2, $team_home_y2);
+		$pdf->Write(0, $row['home_team']);
+
+		// write away team
+		$pdf->SetFont('Helvetica', 'B', 12);
+		$pdf->SetXY($team_away_x2, $team_away_y2);
+		$pdf->Write(0, $row['visit_team']);
+	}
+
+	// 8-Ball Scoresheet
+	if ($league_type == "8-Ball") {
+
+		// add a page
+		$pdf->AddPage();
+
+		// import page 1
+		$tplIdx = $pdf->importPage(1);
+
+		// use the imported page
+		$pdf->useTemplate($tplIdx, 0, 0, 216);
+
+		// write game date
+		$pdf->SetFont('Helvetica', 'B', 10);
+		$pdf->SetXY($game_date_x, $game_date_y);
+		$pdf->Write(0,  $row['week']);
+
+		// write home team
+		$pdf->SetFont('Helvetica', 'B', 12);
+		$pdf->SetXY($team_home_x, $team_home_y);
+		$pdf->Write(0, $row['home_team']);
+	
+		// write away team
+		$pdf->SetFont('Helvetica', 'B', 12);
+		$pdf->SetXY($team_away_x, $team_away_y);
+		$pdf->Write(0, $row['visit_team']);
+	
+		// add a page
+		$pdf->AddPage();
+
+		// import page 2
+		$tplIdx2 = $pdf->importPage(2);
+		$pdf->useTemplate($tplIdx2, 0, 0, 216);
+
+		// write home team
+		$pdf->SetFont('Helvetica', 'B', 12);
+		$pdf->SetXY($team_home_x2, $team_home_y2);
+		$pdf->Write(0, $row['home_team']);
+	
+		// write away team
+		$pdf->SetFont('Helvetica', 'B', 12);
+		$pdf->SetXY($team_away_x2, $team_away_y2);
+		$pdf->Write(0, $row['visit_team']);
+
+		// write home team list
+		$pl_home_y = $players_home_y;
+		while ($hp_row = mysql_fetch_assoc($hp_result)) {
+			$pdf->SetFont('Helvetica', '', 10);
+			$pdf->SetXY($players_home_x, $pl_home_y);
+			$pdf->Write(0, $hp_row['player_name']);
+			$pl_home_y = $pl_home_y + 5; 
+		}
+
+		// write away team list
+		$vl_home_y = $players_away_y;
+		while ($vp_row = mysql_fetch_assoc($vp_result)) {
+			$pdf->SetFont('Helvetica', '', 10);
+			$pdf->SetXY($players_away_x, $vl_home_y);
+			$pdf->Write(0, $vp_row['player_name']);
+			$vl_home_y = $vl_home_y + 5; 
+		}
+	}
+
+	// Scramble League Scoresheet
+	if ($league_type == "9-Ball") {
+
+		// import page 1
+		$tplIdx = $pdf->importPage(1);
+	
+		// add a page
+		$pdf->AddPage('L');
 		
-		while($hp_row = mysql_fetch_assoc($hp_result))
-		{
-			if (($player_page == 2) && (!$team_printed))
-			{
-				$p->setfont($font, 16);
-				$p->set_text_pos( $player['visit'], $x_position);
-				$p->show($hp_row['name']);
-				$x_position -= 18;
-				$team_printed = true;
-			}
-						
-			$p->setfont($font, $player_font_size);
-			$p->set_text_pos( $player['visit'], $x_position);
-			$p->show($hp_row['player_name']);
-			$x_position -= $font_size + $player_spacing;
-		}		
+		// use the imported page 
+		$pdf->useTemplate($tplIdx);
+	
+		// write game date
+		$pdf->SetFont('Helvetica', 'B', 10);
+		$pdf->SetXY($game_date_x, $game_date_y);
+		$pdf->Write(0,  $row['week']);
+	
+		// write home team
+		$pdf->SetFont('Helvetica', 'B', 12);
+		$pdf->SetXY($team_home_x, $team_home_y);
+		$pdf->Write(0, $row['home_team']);
+	
+		// write home team list
+		$pl_home_y = $players_home_y;
+		while ($hp_row = mysql_fetch_assoc($hp_result)) {
+				$pdf->SetFont('Helvetica', '', 10);
+				$pdf->SetXY($players_home_x, $pl_home_y);
+				$pdf->Write(0, $hp_row['player_name']);
+				$pl_home_y = $pl_home_y + 6.6; 
+		}
+	
+		// write away team
+		$pdf->SetFont('Helvetica', 'B', 12);
+		$pdf->SetXY($team_away_x, $team_away_y);
+		$pdf->Write(0, $row['visit_team']);
+	
+		// write away team list
+		$vl_home_y = $players_away_y;
+		while ($vp_row = mysql_fetch_assoc($vp_result)) {
+				$pdf->SetFont('Helvetica', '', 10);
+				$pdf->SetXY($players_away_x, $vl_home_y);
+				$pdf->Write(0, $vp_row['player_name']);
+				$vl_home_y = $vl_home_y + 6.7; 
+		}
+		// import page 2
+		$tplIdx = $pdf->importPage(2);
+	
+		// add a page
+		$pdf->AddPage('L');
+	
+		// use the imported page
+		$pdf->useTemplate($tplIdx);
 	}
-	catch(Exception $e)
-	{
-		die('exception::' . $e);	
-	}
-			
-	$p->end_page_ext("");
 }
 
-$p->end_document("");
 
-$document = $p->get_buffer();
+//ob_end_clean();
+$pdf->Output();
 
-header("Content-Type:application/pdf");
-header("Content-Length:" . strlen($document));
-header("Content-Disposition:inline; filename=scoresheet.pdf");
-echo $document;
 ?>
---
+++ include/mysql_dev.php
@@ -0,0 +1,4 @@
+<?php
+$conn = mysql_connect('localhost', 'leaguedevuser', 'Society2014!');
+mysql_select_db('leaguedev', $conn);
+?>
