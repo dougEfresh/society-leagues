3823eb0f51f0d5261fec0c2ce9fe556d3847d3e3
Luis Guzman <luisg0015@gmail.com>
2014-11-17 23:20:23 -0500

    BCA Stats.

--- admin/bca_stats/bca_stats.php
+++ admin/bca_stats/bca_stats.php
@@ -5,22 +5,60 @@ include '../../include/xtemplate.class.php';
 
 $view = new xtemplate('bca_stats.html');
 
-$result = mysql_query("SELECT player.*, league.*,
-result_team.match_id, result_team.team_id, result_ind.player_id, team.name team_name,
-CONCAT(player.first_name,' ',player.last_name) player_name,
+$resultleagues = mysql_query("SELECT * FROM division 
+						JOIN league ON league.league_id=division.league_id
+						WHERE division_id='{$_GET['division_id']}'");
+
+$row = mysql_fetch_assoc($resultleagues);
+$leagueID = $row['league_id'];
+$scramble = 0;
+
+switch($leagueID)
+{
+	case '1':
+		$hc = "hcd_id=player.hc_9";
+	break;
+	
+	case '2':
+		$hc = "hcd_id=player.hc_8";
+	break;
+	
+	case '3':
+		$hc = "hcd_id=player.hc_straight";
+	break;
+	
+	case '4':
+		$hc = "hcd_id=player.hc_m9";
+		$hc8 = "hcm8.hcd_id=player.hc_m8";
+		$scramble = 1; 
+	break;
+
+	case '5':
+		$hc = "hcd_id=player.hc_10";
+	break;
+		
+	case '6':
+		$hc = "hcd_id=player.hc_8Begin";
+	break;	
+}
+
+$result = mysql_query("SELECT CONCAT(player.first_name,' ',player.last_name) player_name, player.*, handicap_display.*,
+team.name team_name,
 COUNT(result_ind.result_id) matches,
-SUM(result_ind.is_win) wins,
-SUM(IF(result_ind.is_win,0,1)) losses
-FROM division 
-JOIN match_schedule ON match_schedule.division_id=division.division_id
-JOIN result_team ON result_team.match_id=match_schedule.match_id
-JOIN result_ind ON result_ind.match_id=match_schedule.match_id
-JOIN team ON team.team_id=result_team.team_id
-JOIN player ON player.player_id=result_ind.player_id
-JOIN league ON league.league_id=division.league_id
-WHERE division.division_id='{$_GET['division_id']}'
-GROUP BY result_ind.player_id
-ORDER BY team.team_id");
+SUM(result_ind.is_win) wins, (COUNT(result_ind.result_id) - SUM(result_ind.is_win)) losses,
+(ROUND(SUM(result_ind.is_win) / COUNT(result_ind.result_id),3)) AS percentage
+FROM team_player
+RIGHT JOIN player ON player.player_id=team_player.tp_player
+LEFT JOIN handicap_display ON {$hc} 
+RIGHT JOIN team ON team.team_id=team_player.tp_team
+LEFT JOIN division ON division.division_id='{$_GET['division_id']}'
+LEFT JOIN match_schedule ON (match_schedule.division_id=division.division_id AND 
+(match_schedule.home_team_id=team_player.tp_team OR match_schedule.visit_team_id=team_player.tp_team))
+LEFT JOIN result_ind ON result_ind.match_id=match_schedule.match_id AND result_ind.player_id=player.player_id AND result_ind.team_id=team.team_id
+WHERE team_player.tp_division='{$_GET['division_id']}'
+AND NOT player.first_name LIKE '%FORFEIT%' AND NOT player.first_name LIKE '%HANDICAP%'
+AND NOT player.last_name LIKE '%FORFEIT%' AND NOT player.last_name LIKE '%HANDICAP%'
+GROUP BY tp_player ORDER BY name ASC, player_name ASC");
 
 $text = '';
 
