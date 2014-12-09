4cbe1343ddfc32b8af7322846ab90783e213845f
Douglas Chimento <douglas.chimento@yodle.com>
2014-12-09 16:39:56 -0500

    #1 Adding ENV DB_HOST to detemine db hostname

--- include/mysql.php
+++ include/mysql.php
@@ -1,5 +1,10 @@
 <?php
-$conn = mysql_connect('localhost', 'league');
-mysql_select_db('league', $conn);
+if (getenv('DB_HOST')) {
+   $conn = mysql_connect(getenv('DB_HOST'), 'league');
+   mysql_select_db('league', $conn);
+} else {
+   $conn = mysql_connect('localhost', 'league');
+   mysql_select_db('league', $conn);
+}
 
 ?>
