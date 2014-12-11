2a311e475f60d2539424d81064e775734d48f5ef
Douglas Chimento <douglas.chimento@yodle.com>
2014-12-11 18:40:41 -0500

    #1 #2 adding session handler to write in mysql db

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
 ./build.sh
-dockerRev=`docker build -q   .   | grep Successfully | awk '{print $3}'`
+dockerRev=`docker build -q   . | grep Successfully | awk '{print $3}'`
 docker run -d  -p 127.0.0.1:8888:80 $dockerRev 
 
--- system/do_login.php
+++ system/do_login.php
@@ -1,4 +1,19 @@
 <?php
+require_once('../include/session/sessionHandler.php');
+$session = new MySqlSessionHandler();
+
+// add db data
+$session->setDbDetails('localhost', 'league', null, 'league');
+$session->setDbTable('session_handler');
+session_set_save_handler(array($session, 'open'),
+                         array($session, 'close'),
+                         array($session, 'read'),
+                         array($session, 'write'),
+                         array($session, 'destroy'),
+                         array($session, 'gc'));
+// The following prevents unexpected effects when using objects as save handlers.
+register_shutdown_function('session_write_close');
+
 session_start();
 
 include '../include/mysql.php';
@@ -12,6 +27,14 @@ if (mysql_num_rows($result) > 0)
 	$_SESSION['full_name'] = $user['first_name'].' '.$user['last_name'];
 	$_SESSION['first_name'] = $user['first_name'];
 	$_SESSION['last_name'] = $user['last_name'];
+	$sql = sprintf("REPLACE INTO session_map VALUES ('%s','%s')",
+				    $user['player_id'] ,
+				    session_id());
+	if (!(mysql_query($sql))) {
+		   error_log("Could not exec : "  + $sql);
+           throw new Exception(mysql_error());
+    }
+
 	header('Location:../index.php?v=1');
 }
 else
