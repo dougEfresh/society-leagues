c7cc9d215f09a6755ed673d50b0c1952b94af35f
Doug Chimento <doug@dougchimento.com>
2015-01-05 18:40:05 -0500

    Adding foreign key to season

--- schema/season.sql
+++ schema/season.sql
@@ -4,8 +4,9 @@
    name varchar(128) NOT NULL
   `start_date` date NOT NULL,
   `end_date` date NULL
-  `created_date` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `created_date` datetime  NOT NULL DEFAULT CURRENT_TIMESTAMP,
   `updated_date` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
   `status` int(11) DEFAULT '1',
-  PRIMARY KEY (`season_id`)
+  PRIMARY KEY (`season_id`),
+CONSTRAINT FOREIGN KEY (`division_id`) REFERENCES division(division_id) ON DELETE CASCADE ON UPDATE CASCADE
 ) ENGINE=MyISAM AUTO_INCREMENT=4000 DEFAULT CHARSET=latin1;
