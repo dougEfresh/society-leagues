261c018887a885370998d4eba256cb04f8830e9e
Douglas Chimento <dchimento@gmail.com>
2015-03-07 16:07:55 -0500

    Removing @Path in interfaces

--- client-api/src/main/java/com/society/leagues/client/api/admin/TeamAdminApi.java
+++ client-api/src/main/java/com/society/leagues/client/api/admin/TeamAdminApi.java
@@ -3,22 +3,9 @@ package com.society.leagues.client.api.admin;
 
 import com.society.leagues.client.api.domain.Team;
 
-import javax.ws.rs.Consumes;
-import javax.ws.rs.POST;
-import javax.ws.rs.Path;
-import javax.ws.rs.Produces;
-import javax.ws.rs.core.MediaType;
-
-@Path(value = "/")
-@Consumes(MediaType.APPLICATION_JSON)
-@Produces(MediaType.APPLICATION_JSON)
 public interface TeamAdminApi {
 
-    @Path(value = "api/admin/team/create")
-    @POST
     Team create(Team team);
 
-    @Path(value = "api/admin/team/delete")
-    @POST
     Boolean delete(Team team);
 }
