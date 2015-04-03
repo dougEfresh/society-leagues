40a998538ac3a279db9f250c71b4d844b8c3c451
Douglas Chimento <dchimento@gmail.com>
2015-04-03 03:11:48 -0400

    removing old test

--- browserify.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-../node_modules/.bin/browserify -t reactify  -o src/main/resources/public/app/js/bundle.js  src/main/resources/public/jsx/*.jsx  src/main/resources/public/jsx/*/*.jsx  
--- watchify.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash 
-../node_modules/.bin/watchify -v -d -t reactify  -o src/main/resources/public/app/js/bundle.js  src/main/resources/public/jsx/*.jsx  src/main/resources/public/jsx/*/*.jsx  
