8cdf84db313cf39680690a42261da9811211a5dd
Douglas Chimento <dchimento@gmail.com>
2015-04-04 20:14:39 -0400

    Adding node

--- contrib/docker/build-publish.sh
+++ contrib/docker/build-publish.sh
@@ -1,4 +1,4 @@
 git diff-index --quiet HEAD && git pull && ./gradlew clean build && \
-(git submodule  update --remote --force && cd society-leagues-web && git pull && npm install && ./scripts/ugly.sh && rm -rf node_modules) && \
+(git submodule  update --remote --force && cd society-leagues-web && npm install && ./scripts/ugly.sh && rm -rf node_modules) && \
 docker build -t dougefresh/society-leagues-server .
 docker push dougefresh/society-leagues-server:latest
