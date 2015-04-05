78ace1cb98f10eb9aa1ec36b07d149808a730b25
Douglas Chimento <dchimento@gmail.com>
2015-04-04 20:15:45 -0400

    Prevent publish if build fails

--- contrib/docker/build-publish.sh
+++ contrib/docker/build-publish.sh
@@ -1,4 +1,4 @@
 git diff-index --quiet HEAD && git pull && ./gradlew clean build && \
-(git submodule  update --remote --force && cd society-leagues-web && npm install && ./scripts/ugly.sh && rm -rf node_modules) && \
-docker build -t dougefresh/society-leagues-server .
+(git submodule  update --remote --force && cd society-leagues-web && npm install && ./scripts/ugly.sh ) && \
+docker build -t dougefresh/society-leagues-server . && \
 docker push dougefresh/society-leagues-server:latest
