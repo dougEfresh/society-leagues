a5bef3d725ca5ff5c15617907aa4e3fe1291d001
Douglas Chimento <dchimento@gmail.com>
2015-04-04 20:08:36 -0400

    Updating docker build

--- contrib/docker/build-publish.sh
+++ contrib/docker/build-publish.sh
@@ -1,4 +1,4 @@
-git pull && ./gradlew clean build && \
-(cd society-leagues-web && git pull && npm install && ./scripts/ugly.sh && rm -rf node_modules) && \
+git diff-index --quiet HEAD && git pull && ./gradlew clean build && \
+(git submodule  update --remote --force && cd society-leagues-web && git pull && npm install && ./scripts/ugly.sh && rm -rf node_modules) && \
 docker build -t dougefresh/society-leagues-server .
 docker push dougefresh/society-leagues-server:latest
