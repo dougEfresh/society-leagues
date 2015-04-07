cde3f0817f65a82e908569b2167ca41a1461e4dc
Douglas Chimento <dchimento@gmail.com>
2015-04-06 23:33:08 -0400

    Adding name to docker

--- contrib/docker/build-run.sh
+++ contrib/docker/build-run.sh
@@ -1,4 +1,4 @@
 git diff-index --quiet HEAD && git pull && ./gradlew clean build && \
 (git submodule  update --remote --force && cd society-leagues-web && npm install && ./scripts/ugly.sh ) && \
 docker build -t dougefresh/society-leagues-server . && \
-docker  run -v /tmp/logs:/tmp/logs  -d  -p 8000:8000  -u www-data dougefresh/society-leagues-server
+docker  run -n society-leagues-server -v /tmp/logs:/tmp/logs  -d  -p 8000:8000  -u www-data dougefresh/society-leagues-server
