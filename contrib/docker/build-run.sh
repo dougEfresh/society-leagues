3a3bd72f7022514f3935273296a181b08362878c
Douglas Chimento <dchimento@gmail.com>
2015-04-13 21:39:05 -0400

    Fixing build-run.sh

--- contrib/docker/build-run.sh
+++ contrib/docker/build-run.sh
@@ -1,4 +1,8 @@
-git diff-index --quiet HEAD && git pull && ./gradlew clean build && \
+docker stop society-leagues-server 
+docker rm society-leagues-server 
+[ -d /tmp/logs ] || mkdir /tmp/logs
+
+git pull && ./gradlew -x test clean build && \
 (git submodule  update --remote --force && cd society-leagues-web && npm install && ./scripts/ugly.sh ) && \
 docker build -t dougefresh/society-leagues-server . && \
 docker  run --name=society-leagues-server -v /tmp/logs:/tmp/logs  -d  -p 8000:8000  -u www-data dougefresh/society-leagues-server
