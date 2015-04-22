5f6684d66ec4f12ef621c6ec5c00ae74cc7fb228
Douglas Chimento <dchimento@gmail.com>
2015-04-22 09:06:48 -0400

    Fxing typo in Docker

--- contrib/docker/start.sh
+++ contrib/docker/start.sh
@@ -3,4 +3,4 @@
 cd /tmp/
 [ -d logs ] || mkdir logs
 nginx -c /etc/nginx/nginx.conf
-java -XX:+PrintGCTimeStamps -XX:+PrintGCDetails -verbose:gc -Xloggc:/tmp/logs/jvmdebug.log -Xm1g -Dembedded=true  -jar /srv/service.jar 
+java -XX:+PrintGCTimeStamps -XX:+PrintGCDetails -verbose:gc -Xloggc:/tmp/logs/jvmdebug.log -Xmx1g -Dembedded=true  -jar /srv/service.jar 
