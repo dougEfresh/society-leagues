c7e3ff80188b142d801c8ed569ede3e6acf2e488
Douglas Chimento <dchimento@gmail.com>
2015-05-04 21:52:37 -0400

    Team endpoint returns list of teams

--- src/main/java/com/society/leagues/adapters/TeamAdapter.java
+++ src/main/java/com/society/leagues/adapters/TeamAdapter.java
@@ -1,12 +1,20 @@
 package com.society.leagues.adapters;
 
+import com.society.leagues.client.api.domain.Season;
 import com.society.leagues.client.api.domain.Team;
+
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
 
 @SuppressWarnings("unused")
 public class TeamAdapter {
     Team team;
+    List<Season> seasons;
 
-    public TeamAdapter(Team team) {
+    public TeamAdapter(Team team, List<Season> seasons) {
+        this.seasons = seasons;
         this.team = team;
     }
 
@@ -15,5 +23,15 @@ public class TeamAdapter {
 
     public Integer getId() {
         return team.getId();
+    }
+
+    public Set<Integer> getSeasons(){
+        Set<Integer> hashSet = new HashSet<>();
+         hashSet.addAll(seasons.stream().map(Season::getId).collect(Collectors.toList()));
+        return hashSet;
+    }
+
+    public String getName() {
+        return team.getName();
     }
 }
--- src/main/java/com/society/leagues/resource/client/TeamResource.java
+++ src/main/java/com/society/leagues/resource/client/TeamResource.java
@@ -1,6 +1,10 @@
 package com.society.leagues.resource.client;
 
+import com.society.leagues.adapters.TeamAdapter;
+import com.society.leagues.client.api.domain.Player;
+import com.society.leagues.client.api.domain.Season;
 import com.society.leagues.client.api.domain.Team;
+import com.society.leagues.dao.PlayerDao;
 import com.society.leagues.dao.TeamDao;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
@@ -8,21 +12,25 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
+import java.util.stream.Collectors;
 
 @RequestMapping(value = "/api")
 @RestController
 public class TeamResource {
 
     @Autowired TeamDao teamDao;
+    @Autowired PlayerDao playerDao;
 
     @RequestMapping(value = "/teams", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
-    public Map<Integer,Team> teams() {
-        Map<Integer,Team> teams = new HashMap<>();
+    public List<TeamAdapter> teams() {
+        List<TeamAdapter> teams = new ArrayList<>();
+        Collection<Player> players = playerDao.get();
+
         for (Team team: teamDao.get()) {
-            teams.put(team.getId(),team);
+            List<Player> teamPlayer = players.stream().filter(p->p.getTeam().equals(team)).collect(Collectors.toList());
+            List<Season> seasonPlayers = teamPlayer.stream().map(Player::getSeason).collect(Collectors.toList());
+            teams.add(new TeamAdapter(team,seasonPlayers));
         }
         return teams;
     }
