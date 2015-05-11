8d88b1d1e987d7abb2be3097d17ddb0c8334e73d
Douglas Chimento <dchimento@gmail.com>
2015-05-11 09:58:41 -0400

    Adding challenge details

--- src/main/java/com/society/leagues/adapters/ChallengeAdapter.java
+++ src/main/java/com/society/leagues/adapters/ChallengeAdapter.java
@@ -1,6 +1,9 @@
 package com.society.leagues.adapters;
 
 import com.society.leagues.client.api.domain.Challenge;
+import com.society.leagues.client.api.domain.Slot;
+import com.society.leagues.client.api.domain.Status;
+import com.society.leagues.client.api.domain.division.DivisionType;
 
 public class ChallengeAdapter {
 
@@ -10,11 +13,31 @@ public class ChallengeAdapter {
         this.challenge = challenge;
     }
 
-     public ChallengeAdapter() {
+    public ChallengeAdapter() {
 
      }
 
     public Integer getId() {
         return challenge.getId();
+    }
+
+    public Slot getSlot() {
+        return challenge.getSlot();
+    }
+
+    public Integer getChallenger() {
+        return challenge.getChallenger().getUserId();
+    }
+
+    public DivisionType getGame() {
+        return challenge.getChallenger().getDivision().getType();
+    }
+
+    public Integer getOpponent() {
+        return challenge.getOpponent().getUserId();
+    }
+
+    public Status getStatus() {
+        return challenge.getStatus();
     }
 }
