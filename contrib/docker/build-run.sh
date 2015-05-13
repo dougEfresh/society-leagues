7fca3264ed8801a96e1474067ceca8f14db2fd85
Douglas Chimento <dchimento@gmail.com>
2015-05-12 21:29:01 -0400

    Adding env option to docker run

--- contrib/docker/build-run.sh
+++ contrib/docker/build-run.sh
@@ -1,8 +1,12 @@
+set -x 
 docker stop society-leagues-server 
 docker rm society-leagues-server 
 [ -d /tmp/logs ] || mkdir /tmp/logs
+ENV_FILE=""
+
+if [ -n "$1" ] ; then ENV_FILE="--env-file=$1" ; fi
 
 git pull && ./gradlew -x test clean build && \
 (git submodule  update --remote --force && cd society-leagues-web && npm install && ./scripts/ugly.sh ) && \
 docker build -t dougefresh/society-leagues-server . && \
-docker  run --name=society-leagues-server -v /tmp/logs:/tmp/logs  -d  -p 8000:8000  -u www-data dougefresh/society-leagues-server
+docker  run $ENV_FILE --name=society-leagues-server -v /tmp/logs:/tmp/logs  -d  -p 8000:8000  -u www-data dougefresh/society-leagues-server
