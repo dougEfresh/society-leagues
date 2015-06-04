b06ecbf2ff57b78c90e5fea8076cc96b70d3d761
Douglas Chimento <douglas.chimento@yodle.com>
2015-06-04 18:14:08 -0400

    adding challenge user create endpoint

--- src/main/java/com/society/leagues/resource/admin/UserAdminResource.java
+++ src/main/java/com/society/leagues/resource/admin/UserAdminResource.java
@@ -3,6 +3,7 @@ package com.society.leagues.resource.admin;
 import com.society.leagues.adapters.UserAdapter;
 import com.society.leagues.client.api.domain.User;
 import com.society.leagues.dao.UserDao;
+import com.society.leagues.resource.client.ChallengeResource;
 import com.society.leagues.resource.client.UserResource;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
@@ -13,6 +14,7 @@ import org.springframework.web.bind.annotation.*;
 public class UserAdminResource {
     @Autowired UserDao userDao;
     @Autowired UserResource userResource;
+    @Autowired ChallengeResource challengeResource;
 
     @RequestMapping(value = "/user/create/{id}",
             method = RequestMethod.POST,
@@ -26,5 +28,15 @@ public class UserAdminResource {
         return userResource.get(returned.getId());
     }
 
-
+  @RequestMapping(value = "/user/create/{id}/challenge",
+            method = RequestMethod.POST,
+            produces = MediaType.APPLICATION_JSON_VALUE,
+            consumes = MediaType.ALL_VALUE)
+    public UserAdapter createChallengeUser(@PathVariable Integer id ,@RequestBody User user) {
+        User returned = userDao.create(user);
+        if (returned == null) {
+            return null;
+        }
+      return challengeResource.signup(returned.getId());
+    }
 }
