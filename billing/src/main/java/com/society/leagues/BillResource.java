568cb3bb591a6d5dee355dd17127fefb1bb31022
Bob Smith <bob.smith@societybilliards.com>
2015-06-22 17:48:58 -0400

    Adding billing project

---
+++ billing/src/main/java/com/society/leagues/BillResource.java
@@ -0,0 +1,23 @@
+package com.society.leagues;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Map;
+
+@RestController
+public class BillResource {
+
+    @Value("${stripe-key}")
+    String stripApiKey;
+
+    @RequestMapping(value = "/api/v1/billing/challenge/{token}/{userId}",method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
+    public Map<String,Object> challengePayment(@PathVariable String token, @PathVariable Integer id) {
+
+    }
+
+}
---
+++ billing/src/main/java/com/society/leagues/Main.java
@@ -0,0 +1,25 @@
+package com.society.leagues;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan("com.society.leagues")
+@EnableAutoConfiguration
+public class Main implements CommandLineRunner {
+
+    public static void main(String[] args) throws Exception {
+        SpringApplication app = new SpringApplication(Main.class);
+        app.setWebEnvironment(true);
+        app.run(args);
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+    }
+
+}
+
