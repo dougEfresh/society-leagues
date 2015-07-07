914810549ae5eca2fc21730fe7a5e32d0c53c6f4
Douglas Chimento <douglas.chimento@yodle.com>
2015-07-07 11:41:06 -0400

    Adding cache evict

---
+++ webapp/src/main/java/com/society/leagues/CacheClear.java
@@ -0,0 +1,29 @@
+package com.society.leagues;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+
+@Component
+public class CacheClear {
+    public static final int EVICT_TIMEOUT =  60 * 1000 * 5 ;
+    private static Logger logger = LoggerFactory.getLogger(WebMapCache.class);
+    @Autowired List<WebMapCache> webMapCaches;
+    @Autowired List<WebListCache> webListCaches;
+
+    @Scheduled(fixedRate = EVICT_TIMEOUT)
+    public void evict() {
+        logger.info("Evicting Cache");
+        for (WebMapCache webMapCach : webMapCaches) {
+            webMapCach.evict();
+        }
+        for (WebListCache webListCach : webListCaches) {
+            webListCach.evict();
+        }
+    }
+
+}
