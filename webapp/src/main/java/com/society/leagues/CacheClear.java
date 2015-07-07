ea977a89a1768fec6164d7ee0f8c1a6a1df55c26
Bob Smith <bob.smith@societybilliards.com>
2015-07-07 12:26:58 -0400

    Adding token cache endpoint

--- webapp/src/main/java/com/society/leagues/CacheClear.java
+++ webapp/src/main/java/com/society/leagues/CacheClear.java
@@ -3,16 +3,25 @@ package com.society.leagues;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.MediaType;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
 @Component
+@RestController
 public class CacheClear {
     public static final int EVICT_TIMEOUT =  60 * 1000 * 5 ;
     private static Logger logger = LoggerFactory.getLogger(WebMapCache.class);
     @Autowired List<WebMapCache> webMapCaches;
+    @Value("${cache-token:}")
+    String token;
 
     @Scheduled(fixedRate = EVICT_TIMEOUT)
     public void evict() {
@@ -20,6 +29,18 @@ public class CacheClear {
         for (WebMapCache webMapCach : webMapCaches) {
             webMapCach.evict();
         }
+    }
+
+    @RequestMapping(value = "/api/cache/clear/{token}", consumes = MediaType.ALL_VALUE, produces = MediaType.APPLICATION_JSON_VALUE, method = RequestMethod.GET)
+    public Boolean clear(@PathVariable String tokenRequest) {
+        if (token == null || token.isEmpty()) {
+            return false;
+        }
+        if (tokenRequest.equals(token)) {
+            evict();
+            return true;
+        }
+        return false;
     }
 
 }
