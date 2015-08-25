f907e5be4cf512b67aa1bbf4fe7fdbb2e51fb94f
Douglas Chimento <douglas.chimento@yodle.com>
2015-08-25 16:32:53 -0400

    setting sesison timeout

--- webapp/src/main/java/com/society/leagues/conf/spring/SessionConfig.java
+++ webapp/src/main/java/com/society/leagues/conf/spring/SessionConfig.java
@@ -2,16 +2,25 @@ package com.society.leagues.conf.spring;
 
 import org.springframework.boot.context.embedded.ConfigurableEmbeddedServletContainer;
 import org.springframework.boot.context.embedded.EmbeddedServletContainerCustomizer;
+import org.springframework.boot.context.embedded.ServletContextInitializer;
 import org.springframework.boot.context.embedded.tomcat.TomcatEmbeddedServletContainerFactory;
+import org.springframework.context.annotation.Bean;
 import org.springframework.http.HttpStatus;
 
-import java.util.concurrent.TimeUnit;
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
 
-public class SessionConfig  implements EmbeddedServletContainerCustomizer {
+public class SessionConfig  {
 
-    @Override
-    public void customize(ConfigurableEmbeddedServletContainer configurableEmbeddedServletContainer) {
-        TomcatEmbeddedServletContainerFactory tomcat = (TomcatEmbeddedServletContainerFactory) configurableEmbeddedServletContainer;
-        tomcat.setSessionTimeout(90, TimeUnit.DAYS);
-    }
+    @Bean
+    public ServletContextInitializer servletContextInitializer() {
+    return new ServletContextInitializer() {
+        @Override
+        public void onStartup(ServletContext servletContext) throws ServletException {
+            servletContext.getSessionCookieConfig().setMaxAge(Integer.MAX_VALUE/2);
+        }
+    };
+
+}
+
 }
