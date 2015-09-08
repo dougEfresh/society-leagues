79c56b3b03a536ca9c81ef2f933aca0c4642fe7c
Bob Smith <bobsmith1@gmail.com>
2015-09-08 18:02:53 -0400

    Adding lock on updates

--- dao/src/main/java/com/society/leagues/CachedCollection.java
+++ dao/src/main/java/com/society/leagues/CachedCollection.java
@@ -6,13 +6,15 @@ import org.springframework.scheduling.annotation.Scheduled;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.TimeUnit;
 import java.util.concurrent.atomic.AtomicReference;
+import java.util.concurrent.locks.ReentrantLock;
 
 public class CachedCollection<T extends List<? extends LeagueObject>> {
     final AtomicReference<T> entity = new AtomicReference(new ArrayList<T>());
     final MongoRepository repo;
     final String type;
-
+    final ReentrantLock lock = new ReentrantLock();
     public CachedCollection(String type, MongoRepository repo) {
         this.repo = repo; this.type = type;
     }
@@ -27,7 +29,13 @@ public class CachedCollection<T extends List<? extends LeagueObject>> {
 
     @Scheduled(fixedRate = 1000*60*5, initialDelay = 1000*60*15)
     public void update() {
-        this.entity.lazySet((T) repo.findAll());
+        try {
+            lock.tryLock();
+            this.entity.lazySet((T) repo.findAll());
+        } finally {
+            lock.unlock();
+        }
+
     }
 
     public String getType() {
