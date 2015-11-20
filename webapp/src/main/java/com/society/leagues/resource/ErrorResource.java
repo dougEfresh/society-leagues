d30b3f7d41ef2f3115d5e03d50edaa0210b239b9
Bob Smith <bobsmith1@gmail.com>
2015-11-20 17:41:29 -0500

    controller advice

--- webapp/src/main/java/com/society/leagues/resource/ErrorResource.java
+++ webapp/src/main/java/com/society/leagues/resource/ErrorResource.java
@@ -2,6 +2,7 @@ package com.society.leagues.resource;
 
 import org.apache.log4j.Logger;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
 import javax.servlet.http.HttpServletRequest;
@@ -9,14 +10,15 @@ import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
 @Controller
+@ControllerAdvice
 public class ErrorResource {
 
     final static Logger logger = Logger.getLogger(ErrorResource.class);
 
-    @ExceptionHandler(Exception.class)
+    @ExceptionHandler(value = {Exception.class, RuntimeException.class})
     public void handleError(HttpServletRequest req, HttpServletResponse response, Exception exception) throws IOException {
         logger.error(exception.getMessage(),exception);
-        response.sendError(HttpServletResponse.SC_UNAUTHORIZED,"Unknown Error");
+        response.sendError(HttpServletResponse.SC_UNAUTHORIZED, "Unknown Error");
         response.flushBuffer();
     }
 }
