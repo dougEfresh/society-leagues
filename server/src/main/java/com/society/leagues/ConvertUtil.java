a98472a659dfc0d586f373bebb6ea5d27d94b254
Bob Smith <bobsmith1@gmail.com>
2016-01-25 12:04:45 -0500

    purge bad results

--- server/src/main/java/com/society/leagues/ConvertUtil.java
+++ server/src/main/java/com/society/leagues/ConvertUtil.java
@@ -65,6 +65,7 @@ public class ConvertUtil {
     String defaultPassword = new BCryptPasswordEncoder().encode("abc123");
 
     public void convertUser() {
+        /*
         User wrong = leagueService.findByLogin("rtepup@gmail.com");
         User correct = leagueService.findByLogin("rtepub@gmail.com");
         Team wrongTeam = leagueService.findAll(Team.class).stream().filter(t->t.hasUser(wrong)).filter(t->t.isChallenge()).findFirst().get();
@@ -83,5 +84,11 @@ public class ConvertUtil {
         }
         leagueService.purge(wrongTeam);
         leagueService.purge(wrong);
+        */
+
+        List<PlayerResult> bad = leagueService.findAll(PlayerResult.class).stream().filter(p->p.getTeamMatch() == null).collect(Collectors.toList());
+        for (PlayerResult result : bad) {
+            leagueService.purge(result);
+        }
     }
 }
