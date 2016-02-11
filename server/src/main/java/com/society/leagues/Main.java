8eeaec5f90c144bfb7dac9aed3a284ac18f125f5
Bob Smith <bobsmith1@gmail.com>
2016-02-10 21:11:38 -0500

    resetting password

--- server/src/main/java/com/society/leagues/Main.java
+++ server/src/main/java/com/society/leagues/Main.java
@@ -1,16 +1,23 @@
 package com.society.leagues;
 
+import com.society.leagues.client.api.domain.User;
+import com.society.leagues.mongo.UserRepository;
 import com.society.leagues.service.StatService;
+import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.mongo.embedded.EmbeddedMongoAutoConfiguration;
+import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
+import org.springframework.core.env.Environment;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
 
 import java.util.List;
 
@@ -20,9 +27,14 @@ import java.util.List;
 @EnableScheduling
 @EnableAspectJAutoProxy(proxyTargetClass = true)
 public class Main implements CommandLineRunner {
+    private static Logger logger = Logger.getLogger(ConvertUtil.class);
+
     @Autowired ConvertUtil convertUtil;
     @Autowired List<MongoRepository> mongoRepositories;
     @Autowired StatService statService;
+    @Autowired Environment environment;
+    @Autowired UserRepository userRepository;
+    String def = new BCryptPasswordEncoder().encode("abd123");
 
     public static void main(String[] args) throws Exception {
         SpringApplication app = new SpringApplication(Main.class);
@@ -42,7 +54,15 @@ public class Main implements CommandLineRunner {
             }
         }
 
-    }
+        if (environment.acceptsProfiles("dev")) {
 
+            List<User> users = userRepository.findAll();
+            for (User user : users) {
+                user.setPassword(def);
+            }
+            logger.info("Resetting password");
+            userRepository.save(users);
+        }
+    }
 }
 
