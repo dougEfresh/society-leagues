e88d363136b7ed6f4681a63665224dd30e4991cd
Bob Smith <bobsmith@gmail.com>
2016-02-25 19:37:04 -0500

    fb integration

--- src/main/java/com/society/leagues/conf/FilterConfig.java
+++ src/main/java/com/society/leagues/conf/FilterConfig.java
@@ -1,13 +1,12 @@
 package com.society.leagues.conf;
 
-import com.society.leagues.filter.SessionFilter;
 import org.springframework.boot.context.embedded.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class FilterConfig {
-
+    /*
     @Bean
     public FilterRegistrationBean filterRegistrationBean() {
         FilterRegistrationBean filter = new FilterRegistrationBean();
@@ -15,4 +14,5 @@ public class FilterConfig {
         filter.addUrlPatterns("/*");
         return filter;
     }
+    */
 }
