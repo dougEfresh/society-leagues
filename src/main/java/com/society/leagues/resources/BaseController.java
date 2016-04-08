0b76d066f55fdedd1ef1bf26dcf0f2b052e310e4
Bob Smith <bobsmith@gmail.com>
2016-04-08 00:00:56 -0400

    Adding all users

--- src/main/java/com/society/leagues/resources/BaseController.java
+++ src/main/java/com/society/leagues/resources/BaseController.java
@@ -66,7 +66,7 @@ public class BaseController {
         model.addAttribute("challengeSeason",seasons.stream().filter(Season::isChallenge).findFirst().orElse(null));
 
         model.addAttribute("allUsers", userApi.all().parallelStream().filter(User::isReal).collect(Collectors.toList()));
-        model.addAttribute("activeUsers", userApi.all().parallelStream().filter(User::isReal).filter(u->u.getSeasons().stream().filter(Season::isActive).count() > 0).collect(Collectors.toList()));
+        model.addAttribute("activeUsers", userApi.all().parallelStream().filter(User::isReal).collect(Collectors.toList()));
         model.addAttribute("adminSeason",adminSeason);
         model.addAttribute("userTeams", teamApi.userTeams(getUser(model).getId()));
     }
